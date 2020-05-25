54b62d43352502ce046a9607dc4839688f83feaa
kaushik <kaushiknath014@gmail.com>
2020-05-25 21:19:37 +0530

    changes

--- main.js
+++ main.js
@@ -1,34 +1,3 @@
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-});
-
-let buttonInstall = document.querySelector('.install-btn')
-
-buttonInstall.addEventListener('click', (e) => {
-  // Hide the app provided install promotion
-  hideMyInstallPromotion();
-  // Show the install prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    } else {
-      console.log('User dismissed the install prompt');
-    }
-  })
-});
-
-window.addEventListener('appinstalled', (evt) => {
-  console.log('a2hs installed');
-});
 
 window.addEventListener("load", ()=>{
   let lati;
@@ -40,7 +9,7 @@ window.addEventListener("load", ()=>{
       longi = position.coords.longitude;
       lati = position.coords.latitude;
       const proxy ='https://cors-anywhere.herokuapp.com';
-      //const api = `${}`;
+      const api = `${}api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${longi}&appid=${your api key}`;
     });
   };
 });
